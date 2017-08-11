9c779bda299fb294f9ea7ebcaa0a65b83f4acd8d
Larry Jones <larryj@blinds.com>
2017-08-11 11:06:43 -0500

    Grid inside panel

--- 01-supermarket-pricing/src/App.js
+++ 01-supermarket-pricing/src/App.js
@@ -21,24 +21,25 @@ class App extends Component {
               <div className="panel-heading">
                 <h3 class="panel-title">Bean Pricing</h3>
               </div>
-              <div className="panel-body container">
-                <div>
-                  <span>
-                    <label htmlFor="simple-unit-price" className="input-label">Price per can:</label>
-                    <input type="number" id="simple-unit-price" />
-                  </span>
+              <div className="panel-body">
+                <div className="row">
+                  <label htmlFor="simple-unit-price" className="input-label col-md-6">
+                    Price per can:
+                  </label>
+                  <input type="number" id="simple-unit-price col-md-6" />
                 </div>
-                <div>
-                  <span>
-                    <label htmlFor="simple-quantity" className="input-label">Cans:</label>
-                    <input type="number" id="simple-quantity" />
-                  </span>
+                <div className="row">
+                    <label htmlFor="simple-quantity" className="input-label col-md-6">Cans:</label>
+                    <input type="number" id="simple-quantity col-md-6" />
                 </div>
-                <div>
-                  <span>
-                    <label htmlFor="simple-extended-price" className="input-label">Extended price:</label>
-                    <input type="number" id="simple-extended-price" readOnly={true} />
-                  </span>
+                <div className="row">
+                  <label htmlFor="simple-extended-price" className="input-label col-md-6">
+                    Extended price:
+                  </label>
+                  <input type="number"
+                         id="simple-extended-price"
+                         readOnly={true}
+                         className="col-md-6"/>
                 </div>
               </div>
             </div>
